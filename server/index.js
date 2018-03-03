cdc8fff952b1ca239fe40787b3461568d283aaf4
James Lau <james@James-MBP.local>
2018-03-03 14:36:23 -0800

    Node and express

---
+++ server/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/', (req, res) => {
+	res.send({ hi: 'there'});
+})
+
+app.get('/map', (req, res) => {
+	res.send({ hit: 'map'});
+})
+
+app.listen(5000);
